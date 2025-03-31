9593405b1f7b834dd94dd07315c67d50c2cbf6c1
BenjaminChien <ben19990616@gmail.com>
2025-03-31 14:26:13 +0800

    Create Stamp and Photo

---
+++ src/Stamp.jsx
@@ -0,0 +1,7 @@
+function Stamp(){
+    return(
+        <span class='inline-block text-red-600 text-xl font-bold tracking-wider border-3 border-double py-2 px-5 mt-5 mb-5 rotate-[-5deg]'>DECLASSIFIED</span>
+    )
+}
+
+export default Stamp
---
+++ src/assets/basicInfo.js
@@ -0,0 +1,13 @@
+const basicInfo = {
+    name:"Benjamin Chien",
+    title:"Full-stack Developer",
+    birthDate:"1999/06/16",
+    location:"Taipei",
+    language:"Chinese, English",
+    email:"ben19990616@gmail.com",
+    links:{
+        resume:"",
+        linkedIn:"",
+        github:""
+    }
+}
