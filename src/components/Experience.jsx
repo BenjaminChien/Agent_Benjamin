36d020c9a40754735e32f5fe27be977f75f72fbb
BenjaminChien <ben19990616@gmail.com>
2025-04-10 17:34:22 +0800

    Adjust Experience component layout

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,24 @@
+const ExperienceItem = (info) =>{
+    return(
+        <div className="relative pl-8 mb-5">
+            {/*dot */}
+            <div className="w-4 h-4 bg-Beige rounded-full border-2 absolute left-1 top-1"></div>
+            {/*content */}
+            <div>
+                <span className="font-Title italic">{info.info.date}</span>
+                <h3 className="font-Title font-bold text-xl">{info.info.title}</h3>
+                <span className="font-Title block mb-2">{info.info.company}</span>
+                <p className="font-Content">{info.info.description}</p>
+                <ul className="font-Content list-disc pl-5">
+                    {info.info.responsibilities.map((responsibilitie, index)=>(
+                        <li key={index}>{responsibilitie}</li>
+                    ))}
+                </ul>
+            </div>  
+        </div>
+    )
+}
+
 const Experience = (info) => {
     return (
     <div className="mt-5 py-3 w-2xs">
@@ -12,19 +33,18 @@ const Experience = (info) => {
             Experience
         </div>
         
-        {/* Experience list component */}
-        <div>
-            <span className="font-Title italic">{info.info[0].date}</span>
-            <h3 className="font-Title font-bold text-xl">{info.info[0].title}</h3>
-            <span className="font-Title block mb-2">{info.info[0].company}</span>
-            <p className="font-Content">{info.info[0].description}</p>
-            <ul className="font-Content list-disc pl-5">
-                {info.info[0].responsibilities.map((responsibilitie, index)=>(
-                    <li key={index}>{responsibilitie}</li>
-                ))}
-            </ul>
-        </div>  
-    </div>)
+        <div className="relative">
+            {/* dashed line */}
+            <div className="absolute w-0 left-2.5 top-1 bottom-0 border-l-2
+            border-Dark-Gray border-dashed  h-full"></div>
+
+            {info.info.map((item, index)=>(
+                    <ExperienceItem key={index} info={item}></ExperienceItem>
+                ))
+            }
+        </div>
+    </div>
+    )
 }
 
 export default Experience;
